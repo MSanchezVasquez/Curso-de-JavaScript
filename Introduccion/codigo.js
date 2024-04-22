3cc890a5c10943a0edc11dea7be65c08f81f4658
Moises Sanchez <moisesup.25@gmail.com>
2024-04-21 22:45:06 -0500

    intro

---
+++ Introduccion/codigo.js
@@ -0,0 +1,29 @@
+alert("Hola desde el archivo codigo.js");
+
+/*const numero = 3.14;
+numero = 25.1;
+alert(numero);*/
+
+let numero1, numero2, numero3;
+numero1=1;
+numero2=2;
+numero3=3;
+
+//Pruebas con prompt
+let nombre = prompt("Decime tu name: ")
+alert("Hola "+nombre);
+
+//Operadores de asignacion
+let num = 10;
+num += 5;
+document.write(num);
+//Operadores aritmeticos
+let numero5 = 50;
+numero5--;
+alert(numero5);
+let resultado = numero5**2;
+alert(resultado);
+
+let nombre2 = "Moises Sanchez";
+let frase2 = `Soy {nombre} y soy programador`
+
